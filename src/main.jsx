52fdbbb6913db25d36f8024b48f84ef04b94fd3d
Sebastian Upegui <sebastianupegui1@gmail.com>
2025-12-09 16:51:15 -0500

    cambio de logo pestaña

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
 import { AuthProvider } from "./contexts/AuthContext"
+import logoIco from "./assets/logo-ico.ico"
+
+const favicon = document.querySelector('link[rel="icon"]') || document.createElement("link")
+favicon.setAttribute("rel", "icon")
+favicon.setAttribute("type", "image/x-icon")
+favicon.setAttribute("href", logoIco)
+if (!favicon.parentNode) document.head.appendChild(favicon)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
